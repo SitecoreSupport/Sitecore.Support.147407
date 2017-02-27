d2aace895c67f99a59b030b805dc91feaf7b1d8b
Andrey Komarov <akom@sitecore.net>
2017-02-27 15:29:05 -0500

    Commit patch

---
+++ src/Sitecore.Support.147407/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.147407")]
+[assembly: AssemblyProduct("Sitecore.Support.147407")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.147407/SkipStartAnalytics.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Analytics;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+
+namespace Sitecore.Support.Analytics.Pipelines.StartAnalytics
+{
+    public class TrackerChecker : Sitecore.Analytics.Pipelines.StartAnalytics.CreateTracker
+    {
+        public override void Process(PipelineArgs args)
+        {           
+            if (Tracker.Current == null)
+            {
+                Log.Info("Sitecore.Support.147407: aborting StartAnalytics pipeline since Tracker.Current is not initialized.", this);
+                args.AbortPipeline();
+
+            }            
+        }
+    }
+}
