5941a7e1463a976196c00427fe0fe2427ec8b18f
Pushpraj Ruhal <pur@sitecore.net>
2017-05-19 08:55:27 +0800

    Back ported from 8.2.2 + included additional fix from the following commit : /PD-Products-01/04f6bd69-9382-43de-8a5a-2cd784ac39a3/_git/43226169-4e9d-4acd-8d05-78d255ab73a5/commit/38b026647de439381ea5cbcc2a42e35e613da0e1

---
+++ src/Sitecore.Support.147407/StartTracking.cs
@@ -0,0 +1,39 @@
+﻿namespace Sitecore.Support.Mvc.Analytics.Pipelines.MvcEvents.RequestBegin
+{
+  using Sitecore;
+  using Sitecore.Analytics;
+  using Sitecore.Analytics.Tracking;
+  using Sitecore.Diagnostics;
+  using Sitecore.Mvc.Pipelines.Request.RequestBegin;
+  using Sitecore.Sites;
+  using Sitecore.Xdb.Configuration;
+  using System;
+
+  public class StartTracking : RequestBeginProcessor
+  {
+    public override void Process(RequestBeginArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      if (XdbSettings.Tracking.Enabled)
+      {
+        SiteContext site = Context.Site;
+        if (site != null)
+        {
+          TrackingSiteContext context2 = new TrackingSiteContext(site.SiteInfo);
+          if (context2.EnableTracking)
+          {
+            try
+            {
+              Tracker.StartTracking();
+              bool isActive = Tracker.IsActive;
+            }
+            catch (Exception ex)
+            {
+              Log.Error("Sitecore.Support.147407: Cannot start tracking.", ex, typeof(Tracker));
+            }
+          }
+        }
+      }
+    }
+  }
+}
